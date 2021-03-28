4a2fef6d581532542658005a6970170089540690
Andrew YUrchenko <andrew@wsa.kz>
2021-03-28 22:04:56 +0600

    Изменил иконки в меню администратора.

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -4,19 +4,19 @@
 
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('info') }}'><i class='nav-icon la la-question'></i> Инфо блок</a></li>
 
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('blocktype') }}'><i class='nav-icon la la-question'></i> Блоки продукта</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('blocktype') }}'><i class='nav-icon la la-shapes'></i> Блоки продукта</a></li>
 
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('product') }}'><i class='nav-icon la la-question'></i> Продукция</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('productimage') }}'><i class='nav-icon la la-question'></i> Изображения</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('product') }}'><i class='nav-icon la la-cart-plus'></i> Продукция</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('productimage') }}'><i class='nav-icon la la-images'></i> Изображения</a></li>
 
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('ouradvantage') }}'><i class='nav-icon la la-question'></i> Преимущества</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('ouradvantage') }}'><i class='nav-icon la la-thumbs-up'></i> Преимущества</a></li>
 
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('sliderproduct') }}'><i class='nav-icon la la-question'></i> Слайдер продукции</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('sliderproduct') }}'><i class='nav-icon la la-images'></i> Слайдер продукции</a></li>
 
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('review') }}'><i class='nav-icon la la-question'></i> Отзывы</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('review') }}'><i class='nav-icon la la-comments'></i> Отзывы</a></li>
 
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('about') }}'><i class='nav-icon la la-question'></i> О нас</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('about') }}'><i class='nav-icon la la-address-card'></i> О нас</a></li>
 
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('delivery') }}'><i class='nav-icon la la-question'></i> Доставка и оплата</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('delivery') }}'><i class='nav-icon la la-truck-loading'></i> Доставка и оплата</a></li>
 
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('setting') }}'><i class='nav-icon la la-question'></i> Конфигурация</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('setting') }}'><i class='nav-icon la la-cogs'></i> Конфигурация</a></li>
